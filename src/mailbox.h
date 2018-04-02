5fa2bce019e0ed16e613363d2e03d38e5fe0bfa2
Josef Miegl <josef@miegl.cz>
2018-04-02 17:34:32 +0200

    Update mailbox
    This should fix recent mailbox problems.

--- src/mailbox.h
+++ src/mailbox.h
@@ -27,12 +27,9 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include <linux/ioctl.h>
 
-// Newer kernels (>= 4.1) use major 249, older ones major 100.
-#define MAJOR_NUM_A 249
-#define MAJOR_NUM_B 100
-#define IOCTL_MBOX_PROPERTY _IOWR(MAJOR_NUM_B, 0, char *)
+#define MAJOR_NUM 100
+#define IOCTL_MBOX_PROPERTY _IOWR(MAJOR_NUM, 0, char *)
 #define DEVICE_FILE_NAME "/dev/vcio"
-#define LOCAL_DEVICE_FILE_NAME "mbox"
 
 int mbox_open();
 void mbox_close(int file_desc);
@@ -43,7 +40,7 @@ unsigned mem_free(int file_desc, unsigned handle);
 unsigned mem_lock(int file_desc, unsigned handle);
 unsigned mem_unlock(int file_desc, unsigned handle);
 void *mapmem(unsigned base, unsigned size);
-void *unmapmem(void *addr, unsigned size);
+void unmapmem(void *addr, unsigned size);
 
 unsigned execute_code(int file_desc, unsigned code, unsigned r0, unsigned r1, unsigned r2, unsigned r3, unsigned r4, unsigned r5);
 unsigned execute_qpu(int file_desc, unsigned num_qpus, unsigned control, unsigned noflush, unsigned timeout);
